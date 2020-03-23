0edec73bd1586ed0444c69c09246502cc6ca7ec6
GodDriver <goddriver@yahoo.com>
2020-03-23 16:04:42 +0300

    day 1

---
+++ aoc2019/1/solution.py
@@ -0,0 +1,29 @@
+import math
+
+def the_fuel():
+    """
+    What is the sum of the fuel requirements for all of the modules on your spacecraft?
+    What is the sum of the fuel requirements for all of the modules on your spacecraft
+    when also taking into account the mass of the added fuel?
+    """
+
+    filename = '/home/goddriver/Documents/exercises/aoc2019/1/input'
+    with open(filename) as f:
+        lines = f.readlines()
+
+    sum_of_the_fuel = 0
+    sum_of_the_fuel_with_added = 0
+
+    for line in lines:
+        #part 1
+        sum_of_the_fuel += math.floor(int(line)/3)-2
+        #part 2
+        while int(line) > 5:
+            line = math.floor(int(line)/3)-2
+            sum_of_the_fuel_with_added += int(line)
+
+    print(f"The sum of the fuel requirements for all of the modules on your spacecraft \
+is {sum_of_the_fuel}.")
+    print(f"The sum of the fuel requirements for all of the modules on your spacecraft \
+when also taking into account the mass of the added fuel is {sum_of_the_fuel_with_added}.")
+the_fuel()
