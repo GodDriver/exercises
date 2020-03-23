9a8ee51b0ca1e81b9bdedc815f03f12821d21409
GodDriver <goddriver@yahoo.com>
2020-03-23 11:24:45 +0300

    day 2

--- aoc2015/2/solution.py
+++ aoc2015/2/solution.py
@@ -1,8 +1,9 @@
 def calculation_square_feet_of_wrapping_paper_and_feet_of_ribbon():
     """
-    Вычисление необходимого количества оберточной бумаги и ленты.
+    How many total square feet of wrapping paper should they order?
+    How many total feet of ribbon should they order?
     """
-    filename = '/home/goddriver/Documents/python/AOC2015/2/input'
+    filename = '/home/goddriver/Documents/exercises/aoc2015/2/input'
     with open(filename) as f:
         lines = f.readlines()
 
@@ -13,25 +14,26 @@ def calculation_square_feet_of_wrapping_paper_and_feet_of_ribbon():
         line = line.rstrip()
         numbers = line.split("x")
         numbers = list(map(int, numbers))
-        (length, width, height) = numbers
-        if ((min(numbers) == width and max(numbers) == height)
-            or (min(numbers) == height and max(numbers) == width)):
-            average_of_numbers = length
-        elif ((min(numbers) == length and max(numbers) == height)
-              or (min(numbers) == height and max(numbers) == length)):
-            average_of_numbers = width
+        # l - length, w - width, h - height
+        (l, w, h) = numbers
+        if ((min(l, w, h) == w and max(l, w, h) == h)
+            or (min(l, w, h) == h and max(l, w, h) == w)):
+            average_of_numbers = l
+        elif ((min(l, w, h) == l and max(l, w ,h) == h)
+              or (min(l, w, h) == h and max(l, w, h) == l)):
+            average_of_numbers = w
         else:
-            average_of_numbers = height
-        square_feet_of_slack = (min(length, width, height)
+            average_of_numbers = h
+        square_feet_of_slack = (min(l, w, h)
                                 * average_of_numbers)
-        square_feet_one_box = (2 * ((length * width)
-                                    + (width * height)
-                                    + (length * height))
+        square_feet_one_box = (2 * ((l * w)
+                                    + (w * h)
+                                    + (l * h))
                                + square_feet_of_slack)
         square_feet += square_feet_one_box
-        feet_of_ribbon_one_box = (2 * (min(length, width, height)
+        feet_of_ribbon_one_box = (2 * (min(l, w, h)
                                        + average_of_numbers)
-                                  + length * width * height)
+                                  + l * w * h)
         feet_of_ribbon += feet_of_ribbon_one_box
     print(f"Total square feet of wrapping paper is {square_feet}.")
     print(f"Total feet of ribbon is {feet_of_ribbon}.")
