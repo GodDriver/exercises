6c235176efcd89acde4e024fb29b154e7b8a015d
GodDriver <goddriver@yahoo.com>
2020-03-23 10:52:36 +0300

    day 1

--- aoc2015/1/solution.py
+++ aoc2015/1/solution.py
@@ -1,9 +1,9 @@
 def floor_and_basement():
     """
-    Вычисление этажа из инструкции, взятую Сантой.
-    Вычисление первого входа в подвал.
+    To what floor do the instructions take Santa?
+    What is the position of the character that causes Santa to first enter the basement?
     """
-    filename = '/home/goddriver/Documents/python/AOC2015/1/input'
+    filename = '/home/goddriver/Documents/exercises/aoc2015/1/input'
     with open(filename) as f:
         lines = f.readlines()
 
@@ -16,17 +16,13 @@ def floor_and_basement():
             if el == '(':
                 floor += 1
                 first_enter += 1
-                if floor == -1 and i == 0:
-                    print(f"First enter on the basement was on "
-                          + str(first_enter) + " step.")
-                    i += 1
             elif el == ')':
                 floor -= 1
                 first_enter += 1
-                if floor == -1 and i == 0:
-                    print(f"First enter on the basement was on "
-                          + str(first_enter) + " step.")
-                    i += 1
+            if floor == -1 and i == 0:
+                print(f"First enter the basement was on "
+                      + str(first_enter) + " step.")
+                i += 1
     print(f"Floor in the instructions is " + str(floor) + ".")
 
 floor_and_basement()
