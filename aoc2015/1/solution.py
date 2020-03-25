4f39728ea7329b2a19fe07e3068e3423d88649b1
GodDriver <goddriver@yahoo.com>
2020-03-25 17:14:52 +0300

    day 1

--- aoc2015/1/solution.py
+++ aoc2015/1/solution.py
@@ -1,27 +1,30 @@
+"""Day 1"""
+
 def floor_and_basement():
     """
     To what floor do the instructions take Santa?
-    What is the position of the character that causes Santa to first enter the basement?
+    What is the position of the character that causes Santa to first enter \
+    the basement?
     """
+
     filename = '/home/goddriver/Documents/exercises/aoc2015/1/input'
-    with open(filename) as f:
-        lines = f.readlines()
+    with open(filename) as f_input:
+        lines = f_input.readlines()
 
     floor = 0
     first_enter = 0
     i = 0
 
     for line in lines:
-        for el in line:
-            if el == '(':
+        for element in line:
+            if element == '(':
                 floor += 1
                 first_enter += 1
-            elif el == ')':
+            elif element == ')':
                 floor -= 1
                 first_enter += 1
             if floor == -1 and i == 0:
                 print(f"First enter the basement was on {first_enter} step.")
                 i += 1
     print(f"Floor in the instructions is {floor}.")
-
 floor_and_basement()
