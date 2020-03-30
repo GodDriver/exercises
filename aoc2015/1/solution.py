797802069ec9b159d6d9f2857dc0026ecc7f7d96
GodDriver <goddriver@yahoo.com>
2020-03-30 23:05:23 +0300

    day 1

--- aoc2015/1/solution.py
+++ aoc2015/1/solution.py
@@ -1,30 +1,40 @@
 """Day 1"""
 
-def floor_and_basement():
-    """
-    To what floor do the instructions take Santa?
-    What is the position of the character that causes Santa to first enter \
-    the basement?
-    """
-
-    filename = '/home/goddriver/Documents/exercises/aoc2015/1/input'
-    with open(filename) as f_input:
-        lines = f_input.readlines()
+def get_floor(instructions: str) -> int:
+    """Calculation one instruction."""
 
     floor = 0
-    first_enter = 0
+
+    for instruction in instructions:
+        if instruction == '(':
+            floor += 1
+        elif instruction == ')':
+            floor -= 1
+    return floor
+
+def get_basement(instructions: str) -> int:
+    """Calculation first enter the basement."""
+
+    floor = 0
+    position = 0
     i = 0
 
-    for line in lines:
-        for element in line:
-            if element == '(':
-                floor += 1
-                first_enter += 1
-            elif element == ')':
-                floor -= 1
-                first_enter += 1
-            if floor == -1 and i == 0:
-                print(f"First enter the basement was on {first_enter} step.")
-                i += 1
-    print(f"Floor in the instructions is {floor}.")
-floor_and_basement()
+    for instruction in instructions:
+        if instruction == '(':
+            floor += 1
+        elif instruction == ')':
+            floor -= 1
+        position += 1
+        if floor == -1 and i == 0:
+            i += 1
+            return position
+
+if __name__ == "__main__":
+    filename = '/home/goddriver/Documents/exercises/aoc2015/1/input'
+
+    with open(filename) as f_i:
+        elements = f_i.read()
+
+    print(f"In floor {get_floor(elements)}.")
+    print(f"First enter the basement is at character position \
+{get_basement(elements)}.")
