005524cba71d105da59f229e004277de262c8b57
GodDriver <goddriver@yahoo.com>
2020-03-30 12:32:57 +0300

    day 1

--- aoc2019/1/solution.py
+++ aoc2019/1/solution.py
@@ -1,9 +1,6 @@
 """Day 1"""
 
-import math
-
-
-def the_tyranny_of_the_rocket_equation():
+def the_tyranny_of_the_rocket_equation(filename: None) -> int:
     """
     What is the sum of the fuel requirements for all of the modules on your
     spacecraft?
@@ -11,19 +8,18 @@ def the_tyranny_of_the_rocket_equation():
     spacecraft when also taking into account the mass of the added fuel?
     """
 
-    filename = '/home/goddriver/Documents/exercises/aoc2019/1/input'
-    with open(filename) as f_input:
-        lines = f_input.readlines()
+    with open(filename) as f_i:
+        lines = f_i.readlines()
 
     sum_of_the_fuel = 0
     sum_of_the_fuel_with_added = 0
 
     for line in lines:
         #part 1
-        sum_of_the_fuel += math.floor(int(line)/3) - 2
+        sum_of_the_fuel += int(line)//3 - 2
         #part 2
         while int(line) > 5:
-            line = math.floor(int(line)/3) - 2
+            line = int(line)//3 - 2
             sum_of_the_fuel_with_added += int(line)
 
     print(f"The sum of the fuel requirements for all of the modules on your \
@@ -31,4 +27,7 @@ spacecraft is {sum_of_the_fuel}.")
     print(f"The sum of the fuel requirements for all of the modules on your \
 spacecraft when also taking into account the mass of the added fuel is \
 {sum_of_the_fuel_with_added}.")
-the_tyranny_of_the_rocket_equation()
+
+if __name__ == "__main__":
+    FILE = '/home/goddriver/Documents/exercises/aoc2019/1/input'
+    the_tyranny_of_the_rocket_equation(FILE)
