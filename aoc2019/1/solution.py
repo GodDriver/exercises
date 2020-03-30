fadf9aae676725e33e11e45c4ff8143aefefb308
GodDriver <goddriver@yahoo.com>
2020-03-30 14:28:52 +0300

    day 1

--- aoc2019/1/solution.py
+++ aoc2019/1/solution.py
@@ -1,13 +1,20 @@
 """Day 1"""
 
-def the_tyranny_of_the_rocket_equation(filename: None) -> int:
-    """
-    What is the sum of the fuel requirements for all of the modules on your
-    spacecraft?
-    What is the sum of the fuel requirements for all of the modules on your
-    spacecraft when also taking into account the mass of the added fuel?
-    """
+def get_fuel_one_module(mass: int) -> int:
+    """The fuel for one module."""
+    fuel_one_module = mass // 3 - 2
+    return fuel_one_module
 
+def get_fuel_one_module_with_added(mass: int) -> int:
+    """The fuel for one module with the added fuel."""
+    fuel_one_module_with_added = 0
+    while mass > 5:
+        mass = mass // 3 - 2
+        fuel_one_module_with_added += mass
+    return fuel_one_module_with_added
+
+if __name__ == "__main__":
+    filename = '/home/goddriver/Documents/exercises/aoc2019/1/input'
     with open(filename) as f_i:
         lines = f_i.readlines()
 
@@ -15,19 +22,14 @@ def the_tyranny_of_the_rocket_equation(filename: None) -> int:
     sum_of_the_fuel_with_added = 0
 
     for line in lines:
+        line = int(line)
         #part 1
-        sum_of_the_fuel += int(line)//3 - 2
+        sum_of_the_fuel += get_fuel_one_module(line)
         #part 2
-        while int(line) > 5:
-            line = int(line)//3 - 2
-            sum_of_the_fuel_with_added += int(line)
+        sum_of_the_fuel_with_added += get_fuel_one_module_with_added(line)
 
     print(f"The sum of the fuel requirements for all of the modules on your \
 spacecraft is {sum_of_the_fuel}.")
     print(f"The sum of the fuel requirements for all of the modules on your \
 spacecraft when also taking into account the mass of the added fuel is \
 {sum_of_the_fuel_with_added}.")
-
-if __name__ == "__main__":
-    FILE = '/home/goddriver/Documents/exercises/aoc2019/1/input'
-    the_tyranny_of_the_rocket_equation(FILE)
---
+++ aoc2019/1/test_solution.py
@@ -0,0 +1,33 @@
+"""Test for day 1"""
+
+import unittest
+from solution import get_fuel_one_module, get_fuel_one_module_with_added
+
+
+class SolutionTestCase(unittest.TestCase):
+    """Тесты для 'solution.py'."""
+
+    def test_get_fuel_one_module(self):
+        """Расчет топлива для одного модуля работает правильно?"""
+        fuel_one_module = get_fuel_one_module(12)
+        self.assertEqual(fuel_one_module, 2)
+        fuel_one_module = get_fuel_one_module(14)
+        self.assertEqual(fuel_one_module, 2)
+        fuel_one_module = get_fuel_one_module(1969)
+        self.assertEqual(fuel_one_module, 654)
+        fuel_one_module = get_fuel_one_module(100756)
+        self.assertEqual(fuel_one_module, 33583)
+
+    def test_get_fuel_one_module_with_added(self):
+        """
+        Расчет топлива для одного модуля с добавленным работает правильно?
+        """
+        fuel_one_module_with_added = get_fuel_one_module_with_added(14)
+        self.assertEqual(fuel_one_module_with_added, 2)
+        fuel_one_module_with_added = get_fuel_one_module_with_added(1969)
+        self.assertEqual(fuel_one_module_with_added, 966)
+        fuel_one_module_with_added = get_fuel_one_module_with_added(100756)
+        self.assertEqual(fuel_one_module_with_added, 50346)
+
+if __name__ == "__main__":
+    unittest.main()
