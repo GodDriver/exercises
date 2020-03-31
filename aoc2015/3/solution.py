4abeda89f2df788072c3b7a6e91ee4dcb1cc0155
GodDriver <goddriver@yahoo.com>
2020-03-31 19:46:22 +0300

    day 3

--- aoc2015/3/solution.py
+++ aoc2015/3/solution.py
@@ -1,88 +1,78 @@
 """Day 3"""
 
-def number_of_houses():
-    """
-    How many houses receive at least one present?
-    This year, how many houses receive at least one present?
-    """
+def get_number_of_houses(moves: str) -> int:
+    """Calculation number of houses."""
 
-    filename = '/home/goddriver/Documents/exercises/aoc2015/3/input'
-    with open(filename) as f_input:
-        lines = f_input.readlines()
-    # previous year
-    x_int = 0
-    y_int = 0
-    coordinates_of_houses_prev_year = []
-    coordinates_of_houses_prev_year.append((x_int, y_int))
-    for line in lines:
-        for element in line:
-            if element == '^':
-                y_int += 1
-                coordinates_of_houses_prev_year.append((x_int, y_int))
-            elif element == 'v':
-                y_int -= 1
-                coordinates_of_houses_prev_year.append((x_int, y_int))
-            elif element == '>':
-                x_int += 1
-                coordinates_of_houses_prev_year.append((x_int, y_int))
-            elif element == '<':
-                x_int -= 1
-                coordinates_of_houses_prev_year.append((x_int, y_int))
-    print(f"Number of houses is \
-{len(list(set(coordinates_of_houses_prev_year)))}.")
-    # next year
-    x_int = 0
-    y_int = 0
-    xx_int = 0
-    yy_int = 0
+    x_int, y_int = 0, 0
+    coordinates_of_houses = []
+    coordinates_of_houses.append((x_int, y_int))
+    for move in moves:
+        if move == '^':
+            y_int += 1
+        elif move == 'v':
+            y_int -= 1
+        elif move == '>':
+            x_int += 1
+        elif move == '<':
+            x_int -= 1
+        coordinates_of_houses.append((x_int, y_int))
+    return len(list(set(coordinates_of_houses)))
+
+def get_number_of_houses_next_year(moves: str) -> int:
+    """Calculation number of houses next year."""
+
+    xs_int, ys_int = 0, 0
+    xr_int, yr_int = 0, 0
     i = 0
     coordinates_of_houses_santa = []
     coordinates_of_houses_robo_santa = []
-    coordinates_of_houses_next_year = []
-    coordinates_of_houses_santa.append((x_int, y_int))
-    coordinates_of_houses_robo_santa.append((xx_int, yy_int))
+    coordinates_of_houses_santa.append((xs_int, ys_int))
+    coordinates_of_houses_robo_santa.append((xr_int, yr_int))
 
-    for line in lines:
-        for element in line:
-            if element == '^':
-                if i % 2 == 0:
-                    y_int += 1
-                    coordinates_of_houses_santa.append((x_int, y_int))
-                    i += 1
-                else:
-                    yy_int += 1
-                    coordinates_of_houses_robo_santa.append((xx_int, yy_int))
-                    i += 1
-            elif element == 'v':
-                if i % 2 == 0:
-                    y_int -= 1
-                    coordinates_of_houses_santa.append((x_int, y_int))
-                    i += 1
-                else:
-                    yy_int -= 1
-                    coordinates_of_houses_robo_santa.append((xx_int, yy_int))
-                    i += 1
-            elif element == '>':
-                if i % 2 == 0:
-                    x_int += 1
-                    coordinates_of_houses_santa.append((x_int, y_int))
-                    i += 1
-                else:
-                    xx_int += 1
-                    coordinates_of_houses_robo_santa.append((xx_int, yy_int))
-                    i += 1
-            elif element == '<':
-                if i % 2 == 0:
-                    x_int -= 1
-                    coordinates_of_houses_santa.append((x_int, y_int))
-                    i += 1
-                else:
-                    xx_int -= 1
-                    coordinates_of_houses_robo_santa.append((xx_int, yy_int))
-                    i += 1
+    for move in moves:
+        if move == '^':
+            if i % 2 == 0:
+                ys_int += 1
+                coordinates_of_houses_santa.append((xs_int, ys_int))
+            else:
+                yr_int += 1
+                coordinates_of_houses_robo_santa.append((xr_int, yr_int))
+            i += 1
+        elif move == 'v':
+            if i % 2 == 0:
+                ys_int -= 1
+                coordinates_of_houses_santa.append((xs_int, ys_int))
+            else:
+                yr_int -= 1
+                coordinates_of_houses_robo_santa.append((xr_int, yr_int))
+            i += 1
+        elif move == '>':
+            if i % 2 == 0:
+                xs_int += 1
+                coordinates_of_houses_santa.append((xs_int, ys_int))
+            else:
+                xr_int += 1
+                coordinates_of_houses_robo_santa.append((xr_int, yr_int))
+            i += 1
+        elif move == '<':
+            if i % 2 == 0:
+                xs_int -= 1
+                coordinates_of_houses_santa.append((xs_int, ys_int))
+            else:
+                xr_int -= 1
+                coordinates_of_houses_robo_santa.append((xr_int, yr_int))
+            i += 1
 
-    coordinates_of_houses_next_year = (coordinates_of_houses_santa
-                                       + coordinates_of_houses_robo_santa)
+    return len(list(set(coordinates_of_houses_santa
+                        + coordinates_of_houses_robo_santa)))
+
+if __name__ == "__main__":
+    filename = '/home/goddriver/Documents/exercises/aoc2015/3/input'
+
+    with open(filename) as f_input:
+        instructions = f_input.read()
+    # year
+    print(f"Number of houses is {get_number_of_houses(instructions)}.")
+    # next year
     print(f"Number of houses next year is \
-{len(list(set(coordinates_of_houses_next_year)))}.")
-number_of_houses()
+{get_number_of_houses_next_year(instructions)}.")
