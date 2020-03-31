28e9fce23502fc7b1c4e1f875bbb4978fb442bf3
GodDriver <goddriver@yahoo.com>
2020-03-31 16:20:09 +0300

    day 2

--- aoc2015/2/solution.py
+++ aoc2015/2/solution.py
@@ -1,10 +1,43 @@
 """Day 2"""
 
-def calculation_square_feet_of_wrapping_paper_and_feet_of_ribbon():
-    """
-    How many total square feet of wrapping paper should they order?
-    How many total feet of ribbon should they order?
-    """
+def get_square_feet_of_wrapping_paper(sizes: str) -> int:
+    """Calculation square feet of wrapping paper for one box."""
+    sizes = sizes.strip().split("x")
+    sizes = list(map(int, sizes))
+    l, w, h = sizes
+    # l - length, w - width, h - height
+    if ((min(l, w, h) == w and max(l, w, h) == h)
+            or (min(l, w, h) == h and max(l, w, h) == w)):
+        average_of_sizes = l
+    elif ((min(l, w, h) == l and max(l, w, h) == h)
+          or (min(l, w, h) == h and max(l, w, h) == l)):
+        average_of_sizes = w
+    else:
+        average_of_sizes = h
+    square_feet_of_slack = (min(l, w, h) * average_of_sizes)
+    square_feet_one_box = (2 * ((l * w) + (w * h) + (l * h))
+                           + square_feet_of_slack)
+    return square_feet_one_box
+
+def get_feet_of_ribbon(sizes: str) -> int:
+    """Calculation feet of ribbon for one box."""
+    sizes = sizes.strip().split("x")
+    sizes = list(map(int, sizes))
+    l, w, h = sizes
+    # l - length, w - width, h - height
+    if ((min(l, w, h) == w and max(l, w, h) == h)
+            or (min(l, w, h) == h and max(l, w, h) == w)):
+        average_of_sizes = l
+    elif ((min(l, w, h) == l and max(l, w, h) == h)
+          or (min(l, w, h) == h and max(l, w, h) == l)):
+        average_of_sizes = w
+    else:
+        average_of_sizes = h
+    feet_of_ribbon_one_box = (2 * (min(l, w, h) + average_of_sizes)
+                              + l * w * h)
+    return feet_of_ribbon_one_box
+
+if __name__ == "__main__":
     filename = '/home/goddriver/Documents/exercises/aoc2015/2/input'
     with open(filename) as f_input:
         lines = f_input.readlines()
@@ -13,26 +46,7 @@ def calculation_square_feet_of_wrapping_paper_and_feet_of_ribbon():
     feet_of_ribbon = 0
 
     for line in lines:
-        line = line.rstrip()
-        numbers = line.split("x")
-        numbers = list(map(int, numbers))
-        # l - length, w - width, h - height
-        l, w, h = numbers
-        if ((min(l, w, h) == w and max(l, w, h) == h)
-            or (min(l, w, h) == h and max(l, w, h) == w)):
-            average_of_numbers = l
-        elif ((min(l, w, h) == l and max(l, w, h) == h)
-              or (min(l, w, h) == h and max(l, w, h) == l)):
-            average_of_numbers = w
-        else:
-            average_of_numbers = h
-        square_feet_of_slack = (min(l, w, h) * average_of_numbers)
-        square_feet_one_box = (2 * ((l * w) + (w * h) + (l * h))
-                               + square_feet_of_slack)
-        square_feet += square_feet_one_box
-        feet_of_ribbon_one_box = (2 * (min(l, w, h) + average_of_numbers)
-                                  + l * w * h)
-        feet_of_ribbon += feet_of_ribbon_one_box
-    print(f"Total square feet of wrapping paper is {square_feet}.")
-    print(f"Total feet of ribbon is {feet_of_ribbon}.")
-calculation_square_feet_of_wrapping_paper_and_feet_of_ribbon()
+        square_feet += get_square_feet_of_wrapping_paper(line)
+        feet_of_ribbon += get_feet_of_ribbon(line)
+    print(f"They should order {square_feet} square feets of wrapping paper.")
+    print(f"They should order {feet_of_ribbon} feets of ribbon.")
