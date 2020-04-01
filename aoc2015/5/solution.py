a8a0c01cdcdbd17d6ca1f98952fdf7df39738602
GodDriver <goddriver@yahoo.com>
2020-04-01 15:17:27 +0300

    day 5

--- aoc2015/5/solution.py
+++ aoc2015/5/solution.py
@@ -1,55 +1,70 @@
 """Day 5"""
 
-def nice_strings():
-    """
-    How many strings are nice?
-    How many strings are nice under new rules?
-    """
+def get_nice_string(string: str) -> bool:
+    """If string is nice - return True, if not - False."""
 
+    not_contains = ['ab', 'cd', 'pq', 'xy']
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    string = string.lower()
+    if all(not_contain not in string
+           for not_contain in not_contains):
+        count = 0
+        for i in string:
+            for vowel in vowels:
+                if i == vowel:
+                    count += 1
+        if count >= 3:
+            count = 0
+            for i in range(len(string) - 1):
+                if string[i] == string[i + 1]:
+                    count += 1
+            if count > 0:
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+
+def get_nice_string_new_rules(string: str) -> bool:
+    """If string is nice under new rules - return True, if not - False."""
+
+    string = string.lower()
+    count = 0
+    for i in range(len(string) - 2):
+        if string[i] == string[i + 2]:
+            count += 1
+    if count > 0:
+        count = 0
+        for i in range(len(string) - 3):
+            for n_int in range(0, len(string)):
+                if n_int + i < len(string) - 3:
+                    if (string[i] == string[i + 2 + n_int]
+                            and string[i + 1] == string[i + 3 + n_int]):
+                        count += 1
+        if count > 0:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+if __name__ == "__main__":
     filename = '/home/goddriver/Documents/exercises/aoc2015/5/input'
 
     with open(filename) as f_input:
         lines = f_input.readlines()
-    #Old rules
-    not_contains = ['ab', 'cd', 'pq', 'xy']
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    nice_lines = 0
-    for line in lines:
-        line = line.lower()
-        if all(not_contain not in line.lower()
-               for not_contain in not_contains):
-            count = 0
-            for i in line:
-                for vowel in vowels:
-                    if i == vowel:
-                        count += 1
-            if count > 2:
-                count = 0
-                for i in range(len(line) - 1):
-                    if line[i] == line[i + 1]:
-                        count += 1
-                if count > 0:
-                    nice_lines += 1
-    print(f"Nice strings are {nice_lines}.")
 
-    #New rules
-    new_nice_lines = 0
-    for line in lines:
-        line = line.lower()
-        count = 0
-        for i in range(len(line) - 2):
-            if line[i] == line[i + 2]:
-                count += 1
-        if count > 0:
-            count = 0
-            for i in range(len(line) - 3):
-                for n_int in range(0, len(line)):
-                    if n_int + i < len(line) - 2:
-                        if (line[i] == line[i + 2 + n_int]
-                            and line[i + 1] == line[i + 3 + n_int]):
-                            count += 1
-            if count > 0:
-                new_nice_lines += 1
+    nice_lines = 0 #Old rules
+    nice_lines_new_rules = 0 #New rules
 
-    print("Nice strings under new rules are: " + str(new_nice_lines))
-nice_strings()
+    for line in lines:
+        if get_nice_string(line) == 1:
+            nice_lines += 1
+        if get_nice_string_new_rules(line) == 1:
+            nice_lines_new_rules += 1
+
+    print(f"Number of nice strings is {nice_lines}.")
+    print(f"Number of nice strings under new rules is \
+{nice_lines_new_rules}.")
