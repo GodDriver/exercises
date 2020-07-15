a6603900d1712d1fe1632581d6eac3487a790d18
GodDriver <goddriver@yahoo.com>
2020-07-15 23:14:12 +0300

    day 1

---
+++ aoc2018/1/solution.py
@@ -0,0 +1,22 @@
+"""Day 1"""
+
+filename = 'input'
+
+# Part 1
+
+print(sum([int(change) for change in open(filename)]))
+
+# Part 2
+
+change = [int(change) for change in open(filename)]
+frequency = 0
+frequencies = {}
+i = 0
+while True:
+    frequency += change[i % len(change)]
+    i += 1
+    if frequency not in frequencies:
+        frequencies[frequency] = 1
+    else:
+        print(frequency)
+        break
